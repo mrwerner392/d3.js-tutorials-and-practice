c28e886f070a35faf5786c9041713150124be7a9
mrwerner392 <mrwerner392@gmail.com>
2019-12-07 12:09:39 -0500

    bind data to nested selection

--- nested-selections/index.js
+++ nested-selections/index.js
@@ -10,3 +10,21 @@ const td3 = d3.selectAll('tbody tr').selectAll('td') // multiple groups of tds w
 
 // color first column (td index 0 for each tr) red
 td3.style("color", function(d, i) { return i ? null : "red"; });
+
+// 4x4 matrix of data (matches 4 groups of trs with 4 tds each)
+const matrix = [
+  [ 0,  1,  2,  3],
+  [ 4,  5,  6,  7],
+  [ 8,  9, 10, 11],
+  [12, 13, 14, 15],
+];
+
+// binding matrix of data to groups of elements
+// bind entire matrix (array of 4 elements) to selection of trs
+const td = d3.selectAll("tbody tr")
+                .data(matrix)
+              // bind each element of the data (itself an array
+              // of 4 elements) to the subselection of tds
+              // total of 16 data points for 16 tds
+              .selectAll("td")
+                .data(function(d, i) { return d; }); // d is matrix[i]
